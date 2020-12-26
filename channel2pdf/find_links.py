a7106e93b56be401088a17f970cfb30b44a6d108
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-12-26 15:00:45 -0800

    commit

--- channel2pdf/find_links.py
+++ channel2pdf/find_links.py
@@ -11,18 +11,23 @@ asyncio.set_event_loop(loop)
 client = TelegramClient('session_file', credential['api_id'], credential['api_hash'])
 client.start(password=credential['password'])
 
-async def finddLinksAsync(source):
+async def findLinksAsync(source):
     channel_entity=await client.get_entity(source)
-    posts = await client(GetHistoryRequest(peer=channel_entity, limit=1, # change to 30
+    posts = await client(GetHistoryRequest(peer=channel_entity, limit=30, # change to 30
         offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
     return posts
 
 def findLinks(source):
-    print(1)
     loop = asyncio.get_event_loop()
-    print(2)
-    r = loop.run_until_complete(finddLinksAsync(source))
-    print(3)
-    print(r)
-    return {}
+    r = loop.run_until_complete(findLinksAsync(source))
+    links = {}
+    for message in r.messages:
+        try:
+            message.media.webpage
+        except:
+            continue
+        links[tuple([item.url for item in message.entities])] = (
+            message.media.webpage.title[11:-20])
+        print(message.media.webpage.title[11:-20])
+    return links
     
