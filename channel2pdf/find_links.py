d3239e468d922b0350525af2bae5a81884f53687
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-12-26 15:33:40 -0800

    commit

--- channel2pdf/find_links.py
+++ channel2pdf/find_links.py
@@ -6,21 +6,21 @@ import asyncio
 with open('credential') as f:
     credential = yaml.load(f, Loader=yaml.FullLoader)
 
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-client = TelegramClient('session_file', credential['api_id'], credential['api_hash'])
-client.start(password=credential['password'])
-
 async def findLinksAsync(source):
+    client = TelegramClient('session_file', credential['api_id'], credential['api_hash'])
+    await client.start(password=credential['password'])
     channel_entity=await client.get_entity(source)
     posts = await client(GetHistoryRequest(peer=channel_entity, limit=30, # change to 30
         offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
     return posts
 
 def findLinks(source):
+    print(1)
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
+    print(1.5)
     r = loop.run_until_complete(findLinksAsync(source))
+    print(2)
     links = {}
     for message in r.messages:
         try:
