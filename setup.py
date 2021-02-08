a9f0ec3e3464d6cc6b5839a4dd7ccbb20c3bb552
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-02-08 07:45:35 -0800

    commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="channel2pdf",
-    version="0.0.17",
+    version="0.0.18",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Export Telegram Channel to PDF",
